74a7282ef3843d1a52b1f73da0a0e8579bcd2630
Antonio Filho <joseantoniosf84@gmail.com>
2020-09-12 10:27:17 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/recrutamentocompassoapi/RecrutamentoCompassoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.recrutamentocompassoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutamentoCompassoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutamentoCompassoApiApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V01__criar_tabela_cidade.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cidade (
+    codigo BIGINT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    estado VARCHAR(25) NOT NULL,
+    PRIMARY KEY(codigo)
+);
