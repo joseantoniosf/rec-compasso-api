a5de11e5de879d099cc857eb28c727a8dcc227bf
Antonio Filho <joseantoniosf84@gmail.com>
2020-09-12 11:27:03 -0300

    Criacao das tabelas (migracoes)

---
+++ src/main/resources/db/migration/V01__criar_e_popular_estado.sql
@@ -0,0 +1,34 @@
+CREATE TABLE estado (
+    uf VARCHAR(2) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    PRIMARY KEY(uf)
+);
+
+INSERT INTO estado (nome, uf) VALUES ('Acre', 'AC');
+INSERT INTO estado (nome, uf) VALUES ('Alagoas', 'AL');
+INSERT INTO estado (nome, uf) VALUES ('Amapá', 'AP');
+INSERT INTO estado (nome, uf) VALUES ('Amazonas', 'AM');
+INSERT INTO estado (nome, uf) VALUES ('Bahia', 'BA');
+INSERT INTO estado (nome, uf) VALUES ('Ceará', 'CE');
+INSERT INTO estado (nome, uf) VALUES ('Distrito Federal', 'DF');
+INSERT INTO estado (nome, uf) VALUES ('Espírito Santo', 'ES');
+INSERT INTO estado (nome, uf) VALUES ('Gioás', 'GO');
+INSERT INTO estado (nome, uf) VALUES ('Maranhão', 'MA');
+INSERT INTO estado (nome, uf) VALUES ('Mato Grosso', 'MT');
+INSERT INTO estado (nome, uf) VALUES ('Mato Grosso do Sul', 'MS');
+INSERT INTO estado (nome, uf) VALUES ('Minas Gerais', 'MG');
+INSERT INTO estado (nome, uf) VALUES ('Pará', 'PA');
+INSERT INTO estado (nome, uf) VALUES ('Paraíba', 'PB');
+INSERT INTO estado (nome, uf) VALUES ('Paraná', 'PR');
+INSERT INTO estado (nome, uf) VALUES ('Pernambuco', 'PE');
+INSERT INTO estado (nome, uf) VALUES ('Piauí', 'PI');
+INSERT INTO estado (nome, uf) VALUES ('Rio de Janeiro', 'RJ');
+INSERT INTO estado (nome, uf) VALUES ('Rio Grande do Norte', 'RN');
+INSERT INTO estado (nome, uf) VALUES ('Rio Grande do Sul', 'RS');
+INSERT INTO estado (nome, uf) VALUES ('Rondônia', 'RO');
+INSERT INTO estado (nome, uf) VALUES ('Roraima', 'RR');
+INSERT INTO estado (nome, uf) VALUES ('Santa Catarina', 'SC');
+INSERT INTO estado (nome, uf) VALUES ('São Paulo', 'SP');
+INSERT INTO estado (nome, uf) VALUES ('Sergipe', 'SE');
+INSERT INTO estado (nome, uf) VALUES ('Tocantins', 'TO');
+
