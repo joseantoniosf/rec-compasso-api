881e958215c0b8270a4ce0f9c3cd252fbb7b94c7
Antonio Filho <joseantoniosf84@gmail.com>
2020-09-12 13:50:07 -0300

    Cadastro de cidade

--- src/main/java/com/example/recrutamentocompassoapi/resource/CidadeResource.java
+++ src/main/java/com/example/recrutamentocompassoapi/resource/CidadeResource.java
@@ -2,11 +2,21 @@ package com.example.recrutamentocompassoapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.recrutamentocompassoapi.event.RecursoCriadoEvent;
 import com.example.recrutamentocompassoapi.model.Cidade;
 import com.example.recrutamentocompassoapi.repository.CidadeRepository;
 import com.example.recrutamentocompassoapi.repository.filter.CidadeFilter;
@@ -18,8 +28,31 @@ public class CidadeResource {
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	
 	@GetMapping
 	public List<Cidade> pesquisar(CidadeFilter cidadeFilter) {
 		return cidadeRepository.filtrarCidade(cidadeFilter);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Cidade> buscarPeloCodigo(@PathVariable Long codigo) {
+		Cidade cidadeEncontrada = cidadeRepository.findById(codigo).orElse(null);
+		
+		if (cidadeEncontrada != null) {
+			return ResponseEntity.ok(cidadeEncontrada);
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Cidade> criar(@Valid @RequestBody Cidade cidade, HttpServletResponse response) {
+		Cidade cidadeSalva = cidadeRepository.save(cidade);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, cidadeSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(cidadeSalva);
 	}
 }
